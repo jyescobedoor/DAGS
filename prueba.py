0bbb3a73610a4f2be7917252677589fe40770ccc
jyescobedoor <136035058+jyescobedoor@users.noreply.github.com>
2024-05-05 11:42:23 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -11,10 +11,10 @@ local_tz = pendulum.timezone("America/Bogota")
 dag = DAG(
     "My_dag",
     description="prueba dag",
-    start_date=datetime(2024, 5, 10, 00, 00, 00, tzinfo=local_tz),
+    start_date=datetime(2024, 5, 4, 00, 00, 00, tzinfo=local_tz),
     schedule_interval="	0,10,20,30,40,50 * * * *",
     catchup=False,
     tags=["pruebas"],
 )
-op = BashOperator(task_id="hello_world", bash_command="Hello World!", dag=dag)
+op = BashOperator(task_id="hello_world", bash_command="echo Hello World!", dag=dag)
 print(op.retries)  # 3
