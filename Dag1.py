f1eacc49a5f0a277ea65301c3c4041acd9025ca2
jorge.escobedo <jorge.escobedo@telefonica.com>
2024-07-02 01:23:44 -0500

    se actualiza star_date

--- Dag1.py
+++ Dag1.py
@@ -20,7 +20,8 @@ with DAG('dag_dependiente', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=l
         external_dag_id='dag_principal',  # ID del DAG que estás esperando
         external_task_id='task_1',  # ID de la tarea que estás esperando
         allowed_states=['success'],  # Estado que se requiere para proceder
-        timeout=60,  # Tiempo de espera en segundos
+        timeout=None,  # Tiempo de espera en segundos
+        
     )
 
     task2 = DummyOperator(task_id='task_2')
