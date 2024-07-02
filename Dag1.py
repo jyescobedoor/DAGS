5d88fd4d61419ffecf70233f91a6cb4b4c6d2cef
jorge.escobedo <jorge.escobedo@telefonica.com>
2024-07-02 02:11:31 -0500

    se actualiza parametros de scheduler en el dag dependencias

--- Dag1.py
+++ Dag1.py
@@ -10,17 +10,17 @@ from airflow.utils.dates import days_ago
 local_tz = pendulum.timezone("America/Bogota")
 
 # DAG principal
-with DAG('dag_principal', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='5,7 * * * *',catchup=False,) as dag1:
+with DAG('dag_principal', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='13,15 * * * *',catchup=False,) as dag1:
     task1 = DummyOperator(task_id='task_1')
 
 # DAG dependiente
-with DAG('dag_dependiente', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval=None,catchup=False,) as dag2:
+with DAG('dag_dependiente', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='13,15 * * * *',catchup=False,) as dag2:
     wait_for_task_1 = ExternalTaskSensor(
         task_id='wait_for_task_1',
         external_dag_id='dag_principal',  # ID del DAG que estás esperando
         external_task_id='task_1',  # ID de la tarea que estás esperando
         allowed_states=['success'],  # Estado que se requiere para proceder
-        timeout=300,  # Tiempo de espera en segundos
+        timeout=10,  # Tiempo de espera en segundos
 
     )
 
