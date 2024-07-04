b2801d2e2666a10d1ecba24b78138fd26d57a5a9
jorge.escobedo <jorge.escobedo@telefonica.com>
2024-07-04 05:34:32 -0500

    se adiciona scheduler y paramtros al dag

--- Dag1.py
+++ Dag1.py
@@ -10,7 +10,7 @@ from airflow.utils.dates import days_ago
 local_tz = pendulum.timezone("America/Bogota")
 
 # DAG principal
-with DAG('dag_principal', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='13,15 * * * *',catchup=False,) as dag1:
+with DAG('dag_principal', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='5,15,25,35,45,55 * * * *',catchup=False,) as dag1:
     task1 = DummyOperator(task_id='task_1')
     task2 = DummyOperator(task_id='task_2')
     task3 = DummyOperator(task_id='task_3')
@@ -18,14 +18,15 @@ with DAG('dag_principal', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=loc
 
 
 # DAG dependiente
-with DAG('dag_dependiente', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='13,15 * * * *',catchup=False,) as dag2:
+with DAG('dag_dependiente', start_date=datetime(2024, 7, 1, 00, 00, 00, tzinfo=local_tz), schedule_interval='00,10,20,30,40,50 * * * *',catchup=False,) as dag2:
     wait_for_task_3 = ExternalTaskSensor(
         task_id='wait_for_task_3',
         external_dag_id='dag_principal',  # ID del DAG que estás esperando
         external_task_id='task_3',  # ID de la tarea que estás esperando
         allowed_states=['success'],  # Estado que se requiere para proceder
-        timeout=60,  # Tiempo de espera en segundos
-
+        timeout=600,  # Tiempo de espera en segundos
+        mode='reschedule',
+        reschedule_interval=60,
     )
 
     task1 = DummyOperator(task_id='task_1')
