ac481ad0bfdb18b096208a4b203225afa647ffa1
jyescobedoor <136035058+jyescobedoor@users.noreply.github.com>
2026-04-15 14:40:20 -0500

    Update facturacion.py

--- facturacion.py
+++ facturacion.py
@@ -6,7 +6,7 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.ssh.hooks.ssh import SSHHook
 from airflow.providers.standard.operators.python.PythonOperator import PythonOperator
 from airflow.providers.standard.operators.bash.BashOperator import BashOperator
-from airflow.sdk.TaskGroup 
+from airflow.sdk.TaskGroup import TaskGroup
 
 # Instantiate Pendulum and set your timezone.
 local_tz = pendulum.timezone("America/Bogota")
